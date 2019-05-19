92eb076d46f866f6e7bb51238b548731ac88f1d5
lukas1-umbc <lukas1@umbc.edu>
2019-05-18 23:42:36 -0400

    Updated the probability structure distribution a little. Added partial functionality for updating the probabilities based on a sense.

--- reconchess/bots/p5v2_bot.py
+++ reconchess/bots/p5v2_bot.py
@@ -2,65 +2,66 @@ import random
 import pprint
 from reconchess import *
 
-#Dictionary to make it easy/possible to index with the probability distributions.
-#e.g. getIndex("king") will return the king's index in a given square's probability distribution list.
-#This works as long as we initialize the structure correctly.
-#An explanation for these indexes and how the probability ditribution works with them can be found here:
-#https://imgur.com/a/zRJgRnf
+# 8x8x16 list
+distList = [[ [0 for dist in range(6)] for row in range(8)] for col in range(8)]
+# number of pieces the opponent has left
+oppPiecesLeft = 16
+
+# Dictionary to make it easy to index with the probability distributions.
+# e.g. getIndex("king") will return the king's index in a given square's probability distribution list.
+# These indexes are based on those used by pychess
+# https://imgur.com/a/zRJgRnf - note: no longer accurate, but the spirit of the design is still there
 def getIndex(pieceName):
     pieceIndexer = {
-        "rook1": 0, "knight1": 1, "bishop1": 2, 
-        "queen": 3, "king": 4,
-        "bishop2": 5, "knight2": 6, "rook2": 7,
-        "pawn1": 8, "pawn2": 9, "pawn3": 10, "pawn4": 11, "pawn5": 12, "pawn6": 13, "pawn7": 14, "pawn8": 15
+        "pawn": 0, "knight": 1, "bishop": 2, "rook": 3, "queen": 4, "king": 5
     }
     return pieceIndexer[pieceName]
 
-#initializes the 3D list we use to keep the probability distributions of each square
+# initializes the 3D list we use to keep the probability distributions of each square
 def initDist():
-    #8x8x16 list
-    distList = [[ [0 for dist in range(16)] for row in range(8)] for col in range(8)]
 
-    #kinda ugly but it works
+    # kinda ugly but it works
     for col in range(8):
         if col == 0:
-            distList[7][col][getIndex("rook1")] = 1
-            distList[6][col][getIndex("pawn1")] = 1
+            distList[7][col][getIndex("rook")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 1:
-            distList[7][col][getIndex("knight1")] = 1
-            distList[6][col][getIndex("pawn2")] = 1
+            distList[7][col][getIndex("knight")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 2:
-            distList[7][col][getIndex("bishop1")] = 1
-            distList[6][col][getIndex("pawn3")] = 1
+            distList[7][col][getIndex("bishop")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 3:
             distList[7][col][getIndex("queen")] = 1
-            distList[6][col][getIndex("pawn4")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 4:
             distList[7][col][getIndex("king")] = 1
-            distList[6][col][getIndex("pawn5")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 5:
-            distList[7][col][getIndex("bishop2")] = 1
-            distList[6][col][getIndex("pawn6")] = 1
+            distList[7][col][getIndex("bishop")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 6:
-            distList[7][col][getIndex("knight2")] = 1
-            distList[6][col][getIndex("pawn7")] = 1
+            distList[7][col][getIndex("knight")] = 1
+            distList[6][col][getIndex("pawn")] = 1
         elif col == 7:
-            distList[7][col][getIndex("rook2")] = 1
-            distList[6][col][getIndex("pawn8")] = 1
+            distList[7][col][getIndex("rook")] = 1
+            distList[6][col][getIndex("pawn")] = 1
     
     return distList
 
+# goes row by row and prints out each square's distribution - for testing
 def printDist(distList):
 
     for row in range (7, -1, -1):
         for col in range(0, 8):
-            print("|_|", end = " ")
+            print("|", row, ", ", col, "|", end = " ")
             print(distList[row][col])
         print()
 
 class p5v2(Player):
     def handle_game_start(self, color: Color, board: chess.Board):
         distList = initDist()
+        #printDist(distList)
         self.board = board
         self.color = color
         
@@ -70,16 +71,57 @@ class p5v2(Player):
     def choose_sense(self, sense_actions: List[Square], move_actions: List[chess.Move], seconds_left: float) -> Square:
         return random.choice(sense_actions)
 
-    def handle_sense_result(self, sense_result: List[Tuple[Square, Optional[chess.Piece]]]):
-        pass
+    # Board estimate updates should go here
+    def handle_sense_result(self, sense_result: List[Tuple[Square, Optional[chess.Piece]]]):       
+        # add the pieces in the sense result to our board
+        # also want to change probabilities for those spaces to 1 for the appropriate pieces
+        # square is an int between 0 and 63; count from the bottom to bottom top, left to right
+        # for piece, uppercase is white, lowercase is black
+        for square, piece in sense_result:           
+
+            row = square//8
+            col = square - (row * 8)
+            
+            # if the square has a piece
+            if piece is not None:
+                # pawn
+                global distList
+                if piece.piece_type == 1:
+                    distList[row][col][getIndex("pawn")] = 1
+                # knight
+                elif piece.piece_type == 2:
+                    distList[row][col][getIndex("knight")] = 1
+                # bishop
+                elif piece.piece_type == 3:
+                    distList[row][col][getIndex("bishop")] = 1
+                # rook
+                elif piece.piece_type == 4:
+                    distList[row][col][getIndex("rook")] = 1
+                # queen
+                elif piece.piece_type == 5:
+                    distList[row][col][getIndex("queen")] = 1
+                # king
+                elif piece.piece_type == 6:
+                    distList[row][col][getIndex("king")] = 1
+
+            self.board.set_piece_at(square, piece)
+        
 
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
         return random.choice(move_actions + [None])
 
     def handle_move_result(self, requested_move: Optional[chess.Move], taken_move: Optional[chess.Move],
                            captured_opponent_piece: bool, capture_square: Optional[Square]):
-        pass
+        # if we captured a piece, decrement opponent's piece count
+        if captured_opponent_piece:
+            global oppPiecesLeft
+            oppPiecesLeft -= 1
+
+        # if a move was executed, apply it to our board
+        if taken_move is not None:
+            self.board.push(taken_move)
 
     def handle_game_end(self, winner_color: Optional[Color], win_reason: Optional[WinReason],
                         game_history: GameHistory):
+        printDist(distList)
         pass
